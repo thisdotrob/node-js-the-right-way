1b3e8149657854b808ad47c9e0b499dc0720594d
Robert Stevenson <R@192-3-3-8-host.colocrossing.com>
2016-05-15 20:37:51 +0100

    Add net watcher JSON client

---
+++ networking/net-watcher-json-client.js
@@ -0,0 +1,15 @@
+"use strict";
+const
+  net = require('net'),
+  client = net.connect({port: 5432});
+client.on('data', function(data) {
+  let message = JSON.parse(data);
+  if (message.type === 'changed') {
+    let date = new Date(message.timeStamp);
+    console.log("File " + message.file + " changed at " + date);
+  } else if (message.type === 'watching') {
+    console.log("Now watching " + message.file);
+  } else {
+    throw Error("Unrecognised message type: " + message.type)
+  }
+})
