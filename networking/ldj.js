7cf1a6ec9abc552043520bf701a393f0aa5736dc
Rob Stevenson <robstevenson000@gmail.com>
2016-05-15 21:38:03 +0100

    Add logic to LDJClient

--- networking/ldj.js
+++ networking/ldj.js
@@ -4,11 +4,24 @@ const
   events = require('events'),
   util = require('util'),
 
-  //client constructor
+  // client constructor
   LDJClient = function(stream) {
-    //call the EventEmitter constructor on this
+    // call the EventEmitter constructor on this
     events.EventEmitter.call(this);
+    let
+      self = this,
+      buffer = '';
+    stream.on('data', function(data) {
+      buffer += data;
+      let boundary = buffer.indexOf('\n');
+      while (boundary !== -1) {
+        let input = buffer.substr(0, boundary);
+        buffer = buffer.substr(boundary + 1);
+        self.emit('message', JSON.parse(input));
+        boundary = buffer.indexOf('\n');
+      }
+    });
   };
 
-//make LDJClient’s prototypal parent object the EventEmitter prototype.
+// make LDJClient’s prototypal parent object the EventEmitter prototype.
 util.inherits(LDJClient, events.EventEmitter);
