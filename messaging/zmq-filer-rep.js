9e14d20b0b6f7a7bbe9288904fcc4de71c1541f1
Rob Stevenson <robstevenson000@gmail.com>
2016-05-17 08:29:29 +0100

    Add example demonstrating synchronisity in zmq rep

--- messaging/zmq-filer-rep.js
+++ messaging/zmq-filer-rep.js
@@ -1,28 +1,33 @@
 'use strict';
-
 const
   fs = require('fs'),
   zmq = require('zmq'),
+  // socket to reply to client requests
   responder = zmq.socket('rep');
 
+// handle incoming requests
 responder.on('message', function(data) {
+  // parse incoming message
   let request = JSON.parse(data);
-
+  console.log('Received request to get: ' + request.path);
+  // read file and reply with content
   fs.readFile(request.path, function(err, content) {
-    let response = {
+    console.log('Sending response content');
+    responder.send(JSON.stringify({
       content: content.toString(),
       timestamp: Date.now(),
       pid: process.pid
-    };
-    responder.send(JSON.stringify(response));
+    }));
   });
 });
 
-responder.bind('tcp://*:5433', function(err) {
+// listen on TCP port 5433
+responder.bind('tcp://127.0.0.1:5433', function(err) {
   console.log('Listening for zmq requesters...');
 });
 
+// close the responder when the Node process ends
 process.on('SIGINT', function() {
   console.log('Shutting down...');
-  responder.close(); // close the responder when the Node process ends
+  responder.close();
 });
