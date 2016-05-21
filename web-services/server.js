7aa926b13d2c26885bf22f7ae5d430360e5685a2
Rob Stevenson <robstevenson000@gmail.com>
2016-05-21 10:46:18 +0100

    Add simple http server example (non-express)

---
+++ web-services/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+
+server.listen(3000, function() {
+  console.log('Ready, captain!');
+});
