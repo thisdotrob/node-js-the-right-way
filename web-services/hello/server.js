9a8c27ba5e209fedb52491b53f487e091d5a865e
Rob Stevenson <robstevenson000@gmail.com>
2016-05-23 00:10:15 +0100

    Add redis session example in hello

--- web-services/hello/server.js
+++ web-services/hello/server.js
@@ -1,11 +1,18 @@
 #!/Users/R/.nvm/v0.10.20/bin/node --harmony
-
 'use strict';
 
 const express = require('express');
 const app = express();
+const redisClient = require('redis').createClient();
+const RedisStore = require('connect-redis')(express);
 
 app.use(express.logger('dev'));
+
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: 'awesomesecret',
+  store: new RedisStore({ client: redisClient })
+}));
 
 app.get('/api/:name', function(req, res) {
   res.json(200, { 'Hello': req.params.name });
