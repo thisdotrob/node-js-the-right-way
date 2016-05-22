ed2edbe53ac13b9b3334017ec3f25e0ed691e17d
Rob Stevenson <robstevenson000@gmail.com>
2016-05-22 21:24:36 +0100

    Add put book in bundle API path

--- web-services/b4/lib/bundle.js
+++ web-services/b4/lib/bundle.js
@@ -54,7 +54,9 @@ module.exports = function(config, app) {
       .then(onResolved, onRejected).done();
   });
 
-  app.get('/api/bundle/:id/name/:name', function(req, res) {
+  // set the specified bundle's name with the specified name
+  // e.g. curl -X PUT http://localhost:3000/api/bundle/<id>/name/<name>
+  app.put('/api/bundle/:id/name/:name', function(req, res) {
     q.nfcall(request.get, config.b4db + '/' + req.params.id)
       .then(function(args) {
         let couchRes = args[0];
@@ -77,6 +79,49 @@ module.exports = function(config, app) {
         res.json(502, { error: 'bad_gateway', reason: err.code });
       })
       .done();
+  });
+
+  // put a book into a bundle by its id
+  // e.g. curl -X PUT http://localhost:3000/api/bundle/<id>/book/<pgid>
+  app.put('/api/bundle/:id/book/:pgid', function(req, res) {
+
+    let get = q.denodeify(request.get);
+    let put = q.denodeify(request.put);
+
+    q.async(function* (){
+      // grab the bundle from the b4 database
+      let args = yield get(config.b4db + req.params.id);
+      let couchRes = args[0];
+      let bundle = JSON.parse(args[1]);
+
+      // fail fast if we couldn't retrieve the bundle
+      if (couchRes.statusCode !== 200) {
+        res.json(couchRes.statusCode, bundle);
+        return;
+      }
+
+      // look up the book by its Project Gutenberg ID
+      args = yield get(config.bookdb + req.params.pgid);
+      couchRes = args[0];
+      let book = JSON.parse(args[1]);
+
+      // fail fast if we couldn't retrieve the book
+      if (couchRes.statusCode !== 200) {
+        res.json(couchRes.statusCode, book);
+        return;
+      }
+
+      // add the book to the bundle and put it back in CouchDB
+      bundle.books[book._id] = book.title;
+      args = yield put({ url: config.b4db + bundle._id, json: bundle });
+      couchRes = args[0];
+      let body = args[1];
+      res.json(couchRes.statusCode, body);
+
+    })()
+    .catch(function(err) {
+      res.json(502, { error: "bad_gateway", reason: err.code });
+    });
   });
 
 };
